08bdbf052101b4537bbfda0c68713a0ef13c5535
TrinitySlayy <tristi12345@gmail.com>
2025-05-25 15:23:05 +0200

    aller Anfang ist schwer

--- exercises/02_basic_calculator/01_integers/src/lib.rs
+++ exercises/02_basic_calculator/01_integers/src/lib.rs
@@ -1,7 +1,7 @@
 fn compute(a: u32, b: u32) -> u32 {
-    // TODO: change the line below to fix the compiler error and make the tests pass.
-    let multiplier: u8 = 4;
-    a + b * multiplier
+    // TODO: change the line below to fix the comiler error and make the tests pass.
+    let multiplier :u32 = 4;
+    a + multiplier * b
 }
 
 #[cfg(test)]
--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,10 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+    let mut o = 1;
+    for m in 2..=n {
+        o *= m;
+    }
+    o
 }
 
 #[cfg(test)]
